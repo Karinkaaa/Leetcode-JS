46328728e0bea546adff9a93c36c62a01e900906
karinka <95480kiss>
2021-01-25 17:24:09 +0200

    Two Sum

--- test/easy/Task1.test.js
+++ test/easy/Task1.test.js
@@ -1,13 +1,13 @@
 const twoSum = require("../../src/easy/Task1");
 
-test("Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target", () => {
+test("Given an array of integers nums ([2, 7, 11, 15]) and an integer target (9), return indices of the two numbers such that they add up to target", () => {
     expect(twoSum([2, 7, 11, 15], 9)).toStrictEqual([0, 1]);
 });
 
-test("Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target", () => {
+test("Given an array of integers nums ([3, 2, 4]) and an integer target (6), return indices of the two numbers such that they add up to target", () => {
     expect(twoSum([3, 2, 4], 6)).toStrictEqual([1, 2]);
 });
 
-test("Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target", () => {
+test("Given an array of integers nums [3, 3]) and an integer target (6), return indices of the two numbers such that they add up to target", () => {
     expect(twoSum([3, 3], 6)).toStrictEqual([0, 1]);
 });
