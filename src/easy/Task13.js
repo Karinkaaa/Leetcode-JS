da6f5611aa0a0a722f20caac555b8218e48eabbd
karinka <9548026kiss>
2021-02-09 21:12:45 +0200

    Roman to Integer

--- src/easy/Task13.js
+++ src/easy/Task13.js
@@ -4,8 +4,34 @@
  * @param {string} s
  * @return {number}
  */
-const romanToInt = function(s) {
+const romanToInt = function (s) {
+    const roman = {
+        M: 1000,
+        D: 500,
+        C: 100,
+        L: 50,
+        X: 10,
+        V: 5,
+        I: 1
+    };
 
+    let result = 0;
+    let firstNumber = 0;
+    let secondNumber = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        firstNumber = roman[s[i]];
+        secondNumber = roman[s[i + 1]];
+
+        if (firstNumber < secondNumber) {
+            result += (secondNumber - firstNumber);
+            i++;
+        } else {
+            result += firstNumber;
+        }
+    }
+
+    return result;
 };
 
 module.exports = romanToInt;
--- test/easy/Task13.test.js
+++ test/easy/Task13.test.js
@@ -1,11 +1,15 @@
 const romanToInt = require("../../src/easy/Task13");
 
+test("Convert roman numeral 'XVII' to integer", () => {
+    expect(romanToInt("XVII")).toBe(17);
+});
+
 test("Convert roman numeral 'III' to integer", () => {
     expect(romanToInt("III")).toBe(3);
 });
 
 test("Convert roman numeral 'IV' to integer", () => {
-    expect(romanToInt("IV")).toBe(5);
+    expect(romanToInt("IV")).toBe(4);
 });
 
 test("Convert roman numeral 'IX' to integer", () => {
@@ -18,4 +22,16 @@ test("Convert roman numeral 'LVIII' to integer", () => {
 
 test("Convert roman numeral 'MCMXCIV' to integer", () => {
     expect(romanToInt("MCMXCIV")).toBe(1994);
+});
+
+test("Convert roman numeral 'DCXXI' to integer", () => {
+    expect(romanToInt("DCXXI")).toBe(621);
+});
+
+test("Convert roman numeral 'MDCCCLXXXIV' to integer", () => {
+    expect(romanToInt("MDCCCLXXXIV")).toBe(1884);
+});
+
+test("Convert roman numeral 'MDCXCV' to integer", () => {
+    expect(romanToInt("MDCXCV")).toBe(1695);
 });
