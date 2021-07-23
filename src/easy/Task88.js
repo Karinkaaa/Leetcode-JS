9c2709eb6274a9863d2541a0ccc8dcfd7282e420
karina <95480kiss*>
2021-07-23 15:57:59 +0300

    Merge Sorted Array

--- src/easy/Task88.js
+++ src/easy/Task88.js
@@ -8,12 +8,7 @@
  * @return {void} Do not return anything, modify 'nums1' in-place instead
  */
 const merge = function (nums1, m, nums2, n) {
-    if (m > 0) nums1.splice(m);
-    else nums1.splice(0, nums1.length);
-
-    if (n > 0) nums2.splice(n);
-
-    nums1.push(...nums2);
+    nums1.splice(m, n, ...nums2);
     nums1.sort((a, b) => a - b);
 };
 
