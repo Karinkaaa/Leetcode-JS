c85717a7b1f84f63ceac32f751ab9dbf967e0e10
Karinkaaa <kharinakaryna@gmail.con>
2021-09-02 15:35:44 +0300

    Balanced Binary Tree

--- test/easy/Task110.test.js
+++ test/easy/Task110.test.js
@@ -15,7 +15,7 @@ test("Determine if a binary tree [] is height-balanced", () => {
     expect(isBalanced(root)).toBeTruthy();
 });
 
-test("Determine if a binary tree [] is height-balanced", () => {
+test("Determine if a binary tree [1, 2, null, null, 2, 3, null, null, 3, 4, null, null, 4] is height-balanced", () => {
     const root = new TreeNode(1, new TreeNode(2, new TreeNode(3, new TreeNode(4))), new TreeNode(2, null, new TreeNode(3, null, new TreeNode(4))));
     expect(isBalanced(root)).toBeFalsy();
 });
